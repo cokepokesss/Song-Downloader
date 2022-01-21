25d6e96e7a380a1c0bded810b5937fd6436dd734
cokepokesss <98163896+cokepokesss@users.noreply.github.com>
2022-01-22 03:15:30 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    DATABASE = os.environ.get("DB_URI")
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
+    DATABASE = os.environ.get("mongodb+srv://cokepokesmusic:<888999Qq>@cluster0.fvj2o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "2038003838").split())
     SUPPORT = os.environ.get("SUPPORT")
-    BOT_NAME = os.environ.get("bat")
-    BOT_USERNAME = os.environ.get("bn")
+    BOT_NAME = os.environ.get("AdamhakliSong")
+    BOT_USERNAME = os.environ.get("cokepokesmusic_bot")
